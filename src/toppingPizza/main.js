ee7736c4ee9057f3e52c2c7f0a4c283e1addfce8
Patricia Atanasoff <system.informatica365@gmail.com>
2023-10-29 21:06:53 -0300

    crear y remover elementos, eventos

--- src/toppingPizza/main.js
+++ src/toppingPizza/main.js
@@ -151,4 +151,156 @@ console.log(elPrimerTopping.classList.contains('fondo-marron'))
 //Remover una clase
 elPrimerTopping.classList.remove('topping')
 
+//! CREAR y REMOVER ELEMENTOS
+
+const listaToppings = document.getElementById('lista-toppings')// referencia a dónde queremos agregar el elemento
+
+// Crear un elemento
+const toppingNuevo = document.createElement('li')// qué elemento queremos crear
+
+// Para que se muestre antes de apendearlo debemos asignarle estilos y texto
+toppingNuevo.classList.add('topping', 'fondo-marron')
+toppingNuevo.innerText = 'Queso extra'
+
+listaDeToppings.append(toppingNuevo)//agrega un nodo dentro de otro elemento
+
+// Remover un elemento
+toppingNuevo.remove()
+
+// Remover todos los elementos
+//listaToppings.remove()
+
+// Crear un elemento con el método appendChild
+listaToppings.appendChild(toppingNuevo)
+
+//! RECORRER EL DOM
+
+const listaTopping = document.getElementById('lista-toppings')
+
+//  Elementos y nodos padres
+console.log('>Para obtener el elemento padre de algún elemento HTML')
+console.log(listaTopping.parentElement)
+
+console.log('>Podemos ir más arriba en la jerarquía encadenando')
+console.log(listaTopping.parentElement.parentElement)
+
+console.log('>Para obtener el nodo padre de algún elemento HTML')
+console.log(listaTopping.parentNode)
+
+// Elementos y nodos hijos
+
+console.log('>Para obtener el elemento hijo de algún elemento HTML')
+console.log(listaTopping.children)//devuelve una colección HTML
+
+console.log('>Este método trabaja con nodos')
+console.log(listaTopping.firstChild)//por lo que cuenta también los espacios que tenemos en el documento
+
+console.log('>Así mejor usamos children con el índice correspondiente')
+console.log(listaTopping.children[0])
+
+console.log('>Este método trabaja con nodos')
+console.log(listaTopping.lastChild)//= que firstChild cuenta los espacios
+
+
+console.log('> Método más practico')
+console.log(listaTopping.firstElementChild)
+console.log(typeof listaTopping.firstElementChild)
+console.log(listaTopping.lastElementChild)
+console.log(typeof listaTopping.lastElementChild)
+
+console.log('> También se pueden encadenar para ir más adentro en la jerarqía')
+console.log(listaTopping.firstElementChild.firstElementChild)
+
+
+// Elementos y nodos hermanos
+
+console.log('>Para obtener el elemento hermano de algún elemento HTML')
+console.log(listaTopping.previousElementSibling)
+console.log(listaTopping.nextElementSibling)
+
+console.log('>Para obtener el nodo hermano de algún elemento HTML')
+console.log(listaTopping.previousSibling)
+console.log(listaTopping.nextSibling)
+
+//! EVENTOS DEL DOM
+
+//CONCEPTOS IMPORTANTES:
+// - Elemento Target: es el que recibe la acción que desencadena el evento
+// - Trigger: acción que desencadena el evento
+// - Event Handler: Función que va a manejar lo que ocurra cuando se desencadena el evento
+// - Event Listener: escuchador del evento, conexión entre el evento y la función que lo va a manejar
+
+// EVENTOS MÁS USADOS/IMPORTANTES:
+// - lOS QUE INTERACTÚAN CON EL MOUSE
+// - LOS QUE INTERACTÚAN CON EL TECLADO
+
+//* EVENTO ONCLICK
+// Se establece sobre uno o varios elemento/s en el documento html y aquí ejecutamos la función
+
+/*<ul id="lista-toppings">
+            <li onclick ="mostrarClic('aceitunas')" class="topping fondo-marron" id="aceitunas">Aceitunas</li>
+            <li onclick ="mostrarClic('cebolla')" class="topping fondo-naranja">Cebolla</li>
+            <li onclick ="mostrarClic('albahaca')" class="topping fondo-marron" id="albahaca">Albahaca</li>
+            <li onclick ="mostrarClic('champiñones')" class="topping fondo-naranja">Champiñones</li>
+            <!-- Para mostrar como seleccionar por etiqueta. -->
+            <!-- <li>Queso Extra</li> -->
+        </ul>
+ */
+
+/*function mostrarClic(){
+    console.log('Clic')
+}
+
+//Se pueden personalizar los valores pasandole un parámetro a la función y estableciendo el argumento en el onclick del html
+function mostrarClic(topping){
+    console.log(topping)
+}*/
+
+//* addEventListener
+
+console.log('>Evento para un elemento específico del HTML')
+const albahaca = document.getElementById('albahaca')
+
+function mostrarClic(e){
+    console.log(e)// Nos muestra un PointerEvent con todas sus propiedades
+}
+
+function mostrarClic(e){
+    console.log(e.target)//nos muestra el objeto del evento
+}
+
+function mostrarClic(e){
+    console.log(e.target.innerText)//pudiendo ser cada vez más específicos
+}
+
+albahaca.addEventListener('click', mostrarClic)
+
+console.log('>Evento para varios elementos del HTML -colección html')
+const cadaTopping = document.getElementsByClassName('topping')
+
+function mostrarClic(e){
+    console.log(e.target.innerText)
+}
+
+for(const topping of cadaTopping){
+    console.log(topping)
+}
+
+for(const topping of cadaTopping){
+    topping.addEventListener('click', mostrarClic)
+}
+
+// Se puede definir la función dentro del ciclo
+
+for(const topping of cadaTopping){
+    topping.addEventListener('click', (e) =>{
+        console.log(e.target.innerText)
+    })
+}
+
+
+
+
+
+
 
